533d6bdc3ac70e693f31b50a2d550150fc3574c9
stretch1414 <jmoser1414@gmail.com>
2022-03-23 22:51:34 -0400

    Swap to using graphql-helix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
+import {
+  getGraphQLParameters,
+  processRequest,
+  renderGraphiQL,
+  shouldRenderGraphiQL,
+  sendResult,
+} from 'graphql-helix';
 
 import { getToken } from './auth';
 import typeDefs from './typeDefs';
@@ -15,13 +21,15 @@ const schema = makeExecutableSchema({
   resolvers,
 });
 
-const resolveOptions = async (req, res, reqParams) => {
-  return {
-    schema,
-    // context: await context(req, res, reqParams),
-    graphiql: true,
-  };
-};
+app.use(express.json());
+
+// const resolveOptions = async (req, res, reqParams) => {
+//   return {
+//     schema,
+//     context: await context(req, res, reqParams),
+//     graphiql: true,
+//   };
+// };
 
 // app
 //   .use(
@@ -36,7 +44,47 @@ const resolveOptions = async (req, res, reqParams) => {
 //   .unless({ path: ['/token'] });
 
 // Use those to handle incoming requests:
-app.use('/graphql', graphqlHTTP(resolveOptions));
+app.use('/graphql', async (req, res) => {
+  // Create a generic Request object that can be consumed by Graphql Helix's API
+  const request = {
+    body: req.body,
+    headers: req.headers,
+    method: req.method,
+    query: req.query,
+  };
+
+  // Determine whether we should render GraphiQL instead of returning an API response
+  if (shouldRenderGraphiQL(request)) {
+    res.send(renderGraphiQL());
+  } else {
+    // Extract the Graphql parameters from the request
+    const { operationName, query, variables } = getGraphQLParameters(request);
+
+    // Validate and execute the query
+    const result = await processRequest({
+      operationName,
+      query,
+      variables,
+      request,
+      schema,
+    });
+
+    // processRequest returns one of three types of results depending on how the server should respond
+    // 1) RESPONSE: a regular JSON payload
+    // 2) MULTIPART RESPONSE: a multipart response (when @stream or @defer directives are used)
+    // 3) PUSH: a stream of events to push back down the client for a subscription
+    // The "sendResult" is a NodeJS-only shortcut for handling all possible types of Graphql responses,
+    // See "Advanced Usage" below for more details and customizations available on that layer.
+    sendResult(result, res);
+    // if (result.type === "RESPONSE") {
+    //   sendResponseResult(result, res);
+    // } else if (result.type === "MULTIPART_RESPONSE") {
+    //   sendMultipartResponseResult(result, res);
+    // } else if (result.type === "PUSH") {
+    //   sendPushResult(result, res);
+    // }
+  }
+});
 
 // app.get('/token', getToken);
 
