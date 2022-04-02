9ff5a5c4f0f2a8b0cde46d2df5077d0597ac7b5c
stretch1414 <jmoser1414@gmail.com>
2022-04-02 00:26:58 -0400

    Get python execution working

--- src/resolvers/ExecutePython.js
+++ src/resolvers/ExecutePython.js
@@ -1,13 +1,27 @@
+import { cwd } from 'process';
 import { spawn } from 'child_process';
 
 const resolvers = {
   Mutation: {
-    async executePython(obj, args, ctx) {
-      // const process = spawn('python', ['data_example.py']);
-      // // TODO - Stream data back
-      // process.stdout.on('data', (data) => {
-      //   //res.send(data.toString());
-      // });
+    async executePython(obj, { input }, ctx) {
+      const { functionName } = input;
+
+      const proc = spawn('py', ['-u', functionName], {
+        cwd: 'src/lib/python',
+      });
+      proc.stdout.on('data', (data) => {
+        console.log('stdout: ', data.toString());
+      });
+      proc.stderr.on('data', (data) => {
+        console.log('stderr: ', data.toString());
+      });
+
+      proc.on('error', (error) => {
+        console.log('error running child process', error);
+      });
+      proc.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+      });
     },
   },
 };
