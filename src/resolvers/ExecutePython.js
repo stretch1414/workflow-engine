849e1ae96c219206e2b8cd3a3ba0f25f553bf30f
stretch1414 <jmoser1414@gmail.com>
2022-04-05 23:19:47 -0400

    Add buildPercentage mutation

--- src/resolvers/ExecutePython.js
+++ src/resolvers/ExecutePython.js
@@ -30,12 +30,51 @@ const resolvers = {
 
       return pathName;
     },
+    async buildPercentage(obj, { input }, ctx) {
+      const {
+        filename,
+        numeratorColumnName,
+        numeratorColumnValue,
+        denominatorColumnName,
+        denominatorColumnValue,
+      } = input;
+
+      const dataToWrite = [];
+
+      dataToWrite.push('import sys');
+      dataToWrite.push('import pandas as pd');
+      dataToWrite.push('def main():');
+      dataToWrite.push('\tdataset = pd.read_excel(sys.argv[1])');
+      dataToWrite.push('\tdataset = dataset.fillna(0)');
+      dataToWrite.push(
+        `\tnumerator = len(dataset[dataset['${numeratorColumnName}'] == '${numeratorColumnValue}'])`
+      );
+      dataToWrite.push(
+        `\tdenominator = len(dataset[dataset['${denominatorColumnName}'] == '${denominatorColumnValue}'])`
+      );
+      dataToWrite.push(
+        `\twin_percentage = numerator / (numerator + denominator)`
+      );
+      dataToWrite.push("\tprint('win_percentage %s' % win_percentage)");
+      dataToWrite.push('main()');
+
+      const pathName = path.resolve('src/tmp/python', filename);
+
+      fs.writeFile(pathName, dataToWrite.join('\n'), (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+
+      return pathName;
+    },
     async executePython(obj, { input }, ctx) {
       const { functionName, filename } = input;
 
       const returnData = [];
+      const resolvedFilename = path.resolve('src/tmp/database', filename);
       return new Promise((resolve, reject) => {
-        const proc = spawn('py', ['-u', functionName, filename], {
+        const proc = spawn('py', ['-u', functionName, resolvedFilename], {
           cwd: 'src/tmp/python',
         });
 
